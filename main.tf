06cd2697a9e32ac5ccfce56a67c5437baedaa494
Carlos Eduardo Lima <kduchagas.lima@gmail.com>
2020-08-05 19:09:23 -0300

    teste terraform

--- main.tf
+++ main.tf
@@ -8,30 +8,32 @@ resource "google_compute_instance_template" "vm_instance"{
   name = "teste-terraform"
   machine_type = "n1-standard-1"
   metadata_startup_script = "yum update && yum install -y ansible"
-  //boot_disk{
-  //  initialize_params{
-  //    image = "centos-8-v20200714"
-  //  }
-  //}
+  
   network_interface{
     network = "${google_compute_network.vpc_network.self_link}"
+    access_config {
+      nat_ip = "${google_compute_address.static.address}"
+    }     
   }
-}
   disk {
     source_image = "centos-8-v20200714"
     auto_delete = true
     boot = true
-  }
+  }  
+resource "google_compute_address" "static" {
+  name = "ipv4-address"
+}
 resource "google_compute_network" "vpc_network" {
   name = "vpc-network"
 }
-resource "google_compute_instance_group_manager" "teste-terraform"{
+resource "google_compute_instance_group_manager" "teste-terraform" {
   name = "laboratorio"
-  instance_template = "${google_compute_instance_template.vm_instance.self_link}"
   base_instance_name = "lab-terraform"
   zone = "us-central1-c"
-  target_size = 3 
-
+  target_size = 3
+  version {
+    instance_template = "${google_compute_instance_template.vm_instance.self_link}"
+  }   
 }
 # Cria o Firewall para a VM
 resource "google_compute_firewall" "webfirewall" {
@@ -42,4 +44,5 @@ resource "google_compute_firewall" "webfirewall" {
     protocol = "tcp"
     ports = [22]
   }
+}
 }
